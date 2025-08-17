6ab2a6ec32e5ceb716eb2ff4db6dfccd6866409d
darelldeejay <67309679+darelldeejay@users.noreply.github.com>
2025-08-17 13:30:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
 import requests
 
-# Leer variables de entorno
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+def enviar_telegram(mensaje):
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_USER_ID")
 
-def enviar_mensaje_telegram(mensaje):
     if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
         print("❌ Variables de entorno para Telegram no configuradas.")
-        print(f"TELEGRAM_TOKEN: {TELEGRAM_TOKEN}")
-        print(f"TELEGRAM_CHAT_ID: {TELEGRAM_CHAT_ID}")
+        print(f"TELEGRAM_BOT_TOKEN: {TELEGRAM_TOKEN}")
+        print(f"TELEGRAM_USER_ID: {TELEGRAM_CHAT_ID}")
         return
 
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
