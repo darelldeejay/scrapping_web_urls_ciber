93eb7c8a0804d38764864f1adc20d13bc9304dda
darelldeejay <67309679+darelldeejay@users.noreply.github.com>
2025-08-17 13:09:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def enviar_telegram(mensaje):
 
     if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
         print("❌ Variables de entorno para Telegram no configuradas.")
-        print(f"TELEGRAM_TOKEN: {TELEGRAM_TOKEN}")
-        print(f"TELEGRAM_CHAT_ID: {TELEGRAM_CHAT_ID}")
+        print(f"TELEGRAM_BOT_TOKEN: {TELEGRAM_TOKEN}")
+        print(f"TELEGRAM_USER_ID: {TELEGRAM_CHAT_ID}")
         return
 
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
